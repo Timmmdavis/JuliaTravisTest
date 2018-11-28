1a2bf8f45d134bbcc29cf89aae14d6d383abd546
TimmmdavisLaptop <r01tjd16@abdn.ac.uk>
2018-11-28 16:27:28 +0100

    Last commit ran, does it with this?

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,9 +6,11 @@ println("Starting Test 1")
 #Function doubles input arg
 Y=MrFunc(2);
 @test Y==4
+println("Test 1 passed")
 
 println("Starting Test 2")
 ##Test 2, check the external one works (imported into the module script)
 #Function halves input arg
 Y=TravisTest.MrsFunc(8);
 @test Y==4
+println("Test 2 passed")
